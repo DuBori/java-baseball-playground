b3d48a5f612d4a7ca186151358b3c1506c62485e
DuBori <92709439+DuBori@users.noreply.github.com>
2023-07-25 22:21:17 +0900

    자체 refector : 계산기 테스트

--- src/test/java/study/TestOne.java
+++ src/test/java/study/TestOne.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class TestOne {
 
-
     public static String calculator() {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
@@ -17,6 +16,7 @@ public class TestOne {
         String[] strings = s.split(" ");
 
         int result = 0;
+
         try {
             result = Integer.parseInt(strings[0]);
         }catch (NumberFormatException e){
@@ -24,37 +24,26 @@ public class TestOne {
         }
 
         for (int i = 1; i < strings.length; i+=2) {
-             result += cal(strings, i);
+             result = cal(strings, i, result);
         }
         return String.valueOf(result);
     }
 
-    private static int cal(String[] strings, int i) {
+    private static int cal(String[] strings, int i,int result) {
         if("*".equals(strings[i])){
-            return Integer.parseInt(strings[i - 1]) * Integer.parseInt(strings[i + 1]);
+            return result * Integer.parseInt(strings[i + 1]);
         }
         if("/".equals(strings[i])){
-            return Integer.parseInt(strings[i - 1]) / Integer.parseInt(strings[i + 1]);
+            return result / Integer.parseInt(strings[i + 1]);
         }
         if("+".equals(strings[i])){
-            return Integer.parseInt(strings[i - 1]) + Integer.parseInt(strings[i + 1]);
+            return result + Integer.parseInt(strings[i + 1]);
         }
         if("-".equals(strings[i])){
-            return Integer.parseInt(strings[i - 1]) - Integer.parseInt(strings[i + 1]);
+            return result - Integer.parseInt(strings[i + 1]);
         }
         return 0;
     }
-
-    private static boolean isNumber(String param) {
-        try {
-            Integer.parseInt(param);
-        }catch (Exception e){
-            return false;
-        }
-        return true;
-    }
-
-
     public static void main(String[] args) {
         System.out.println("result = "+ calculator());
     }
